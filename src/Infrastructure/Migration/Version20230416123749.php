b86b6d1a4bfc2967e62928acbe3b5a12430021f4
Andrii Vozniuk <a.vozniuk00@gmail.com>
2023-04-16 14:40:07 +0200

    add outbox migration; fix db port

---
+++ src/Infrastructure/Migration/Version20230416123749.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230416123749 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add outbox messages';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE outbox_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_24F454FEFB7336F0 ON outbox_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_24F454FEE3BD61CE ON outbox_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_24F454FE16BA31DB ON outbox_messages (delivered_at)');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_outbox_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'outbox_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON outbox_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON outbox_messages FOR EACH ROW EXECUTE PROCEDURE notify_outbox_messages();');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE outbox_messages');
+    }
+}
